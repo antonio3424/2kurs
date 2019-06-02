5d0fe56910ff81c8a19d490523fa80227fdb5840
antonio3424 <antonio3424@users.noreply.github.com>
2019-06-02 13:37:10 +0300

    Add files via upload

---
+++ 3kurs/Laba 1/src/calc/Calculator.java
@@ -0,0 +1,37 @@
+import java.lang.*;
+import java.util.Scanner;
+
+ public class Calc {
+     public static void main(String[] args) {
+         Scanner sc = new Scanner(System.in);
+         System.out.print("Enter an Operator: ");
+         String in = sc.next();
+         char oper = in.charAt(0);
+
+         System.out.print("Enter a number: ");
+         in = sc.next();
+         double num1 = Double.parseDouble(in);
+
+         System.out.print("Enter another number: ");
+         in = sc.next();
+         double num2 = Double.parseDouble(in);
+
+         if (oper == '+') {
+             double result = num1 + num2;
+             System.out.println(result);
+         } else if (oper == '-') {
+             double result = num1 - num2;
+             System.out.println(result);
+         } else if (oper == '*') {
+             double result = num1 * num2;
+             System.out.println(result);
+         } else if (oper == '/') {
+             double result = num1 / num2;
+             System.out.println(result);
+         } else {
+             double result = num1 % num2;
+             System.out.println(result);
+         }
+
+     }
+ }
